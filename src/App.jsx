901551be8b96136a8543f088a83d33f6e7dcf513
Jayedjb765 <jayedjb76@gmail.com>
2024-06-12 00:03:49 +0600

    Using Api for load data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
 import Header from './Components/Header/Header'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
      
     <Header></Header>
+    <Blogs></Blogs>
     
     </>
   )
