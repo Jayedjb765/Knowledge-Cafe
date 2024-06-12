ca0d12d0d5ba35ed4ffb1a3b2c0ce8f9772ebef2
Jayedjb765 <jayedjb76@gmail.com>
2024-06-12 23:01:35 +0600

    Addig all information related to blog

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,30 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    console.log(blog)
+    const {title,cover,author,author_img,reading_time,hashtag,post_date} =  blog;
     return (
         <div>
+            <img src={cover} alt={`Cover Picture of the ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14 rounded-3xl' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{post_date}</p>
+                    </div>
+
+                </div>
+                <div>
+                    <span>{reading_time} read</span>
+
+                </div>
+            </div>
+            <h2 className="text-4xl">Blogs: {title}</h2>
+            <p>
+                {
+                    hashtag.map((hash,idx) => <span key={idx}><a href=""> #{hash}</a></span>)
+                }
+            </p>
             
         </div>
     );
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs,setBlogs] = useState([]);
@@ -10,8 +11,16 @@ const Blogs = () => {
     },[])
     return (
         <div className="md:w-2/3 ">
-           <h4 className="text-4xl">Blogs</h4>
-            
+           <h4 className="text-5xl">Blogs: {blogs.length}</h4>
+            {
+                blogs.map(blog => <Blog 
+                    key={blog.Id} 
+                    blog={blog}
+                    
+                    >
+
+                    </Blog>)
+            }
         </div>
     );
 };
